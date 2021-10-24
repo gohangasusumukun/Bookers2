140cdc966555ac8f6262b2d9d0fdfa86009e2282
gohangasusumukun <isecha.ocha.cha.sket@gmail.com>
2021-10-24 12:06:02 +0000

    [add] topページ　投稿機能

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,31 @@
 class BooksController < ApplicationController
   def index
+    @book = Book.all
   end
 
   def show
+
   end
 
   def new
+     @book = Book.new
+  end
+
+  def create
+    # １. データを新規登録するためのインスタンス作成
+    book = Book.new(book_params)
+    # ２. データをデータベースに保存するためのsaveメソッド実行
+    book.save
+    # ３. トップ画面へリダイレクト
+    redirect_to '/top'
   end
 
   def edit
   end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_24_083547) do
+ActiveRecord::Schema.define(version: 2021_10_24_090853) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.string "category"
     t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "homes", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
