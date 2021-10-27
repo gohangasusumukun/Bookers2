9dbaf5b71c1d701f8175439702a41f688024f44c
gohangasusumukun <isecha.ocha.cha.sket@gmail.com>
2021-10-27 14:23:16 +0000

    [Update] index show edit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -13,19 +13,56 @@ class BooksController < ApplicationController
 　　# indexに一覧、新規投稿のためなし
   end
 
-　def create
-    @book = Book.new(book_params)
-    @book.save
-    redirect_to book_path(book.id)
 
-　end
+  def create
+    # 作成
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :index
+
+    end
+  end
 
   def show
+    # 詳細へ
     @book = Book.find(params[:id])
   end
 
   def edit
+    # 編集へ
     @book = Book.find(params[:id])
+  end
+
+  def update
+    # アップデート
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    # 削除
+    @book = Book.find(params[:id])
+    if @book.destroy
+      flash[:notice] = "Book was successfully destroyed."
+      redirect_to "/books"
+    else
+　　# indexへ与える
+      render :index
+    end
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,7 @@
 Rails.application.routes.draw do
 
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  get 'top' => 'homes#top'
-
-  get 'books/new'
-
-  get 'top' => 'homes#top'
-  post 'books' => 'books#create'
-  get 'books' => 'books#index'
-  get 'books/:id' => 'books#show', as: 'book'
+  root to: 'homes#top'
+  resources :books
 
 end
